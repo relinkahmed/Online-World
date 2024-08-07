f3350cf57e2b794f2634fa6d4f34971be522f0cc
relinkahmed <www.relink@gmail.com>
2024-08-07 13:09:51 +0300

    Add files via upload

---
+++ phpcode.php
@@ -0,0 +1,16 @@
+<?php
+
+// Set the default timezone
+date_default_timezone_set('UTC');
+
+// Get the current time
+$currentTime = time();
+
+// Format the current time
+$formattedTime = date('H:i:s', $currentTime);
+
+// Display the message
+echo "<p>هذا هو زمن الإسلام وسندعم Relink</p>";
+echo "<p>الوقت الحالttttttttrي: " . $formattedTime . "</p>";
+
+?>
